fd7f36dea429744310069014b5fd3a6989d9ad7b
McAtze <info@it-maku.com>
2018-05-31 18:09:14 +0200

    Update Setup.php

--- upload/src/addons/XenDACH/LimitLanguageUsage/Setup.php
+++ upload/src/addons/XenDACH/LimitLanguageUsage/Setup.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace XenDACH\LimitLanguageUsage;
@@ -32,7 +31,7 @@ class Setup extends AbstractSetup
         });
 
         $schemaManager->alterTable('xf_language', function(\XF\Db\Schema\Alter $table) {
-		// $table->changeColumn('xd_user_selectable', 'bool')->setDefault(1);
+	// $table->changeColumn('xd_user_selectable', 'bool')->setDefault(1);
         });
     }
 
@@ -45,51 +44,3 @@ class Setup extends AbstractSetup
         });
     }
 }
-=======
-<?php
-
-namespace XenDACH\LimitLanguageUsage;
-
-use XF\AddOn\AbstractSetup;
-use XF\AddOn\StepRunnerInstallTrait;
-use XF\AddOn\StepRunnerUninstallTrait;
-use XF\AddOn\StepRunnerUpgradeTrait;
-
-class Setup extends AbstractSetup
-{
-    use StepRunnerInstallTrait;
-    use StepRunnerUpgradeTrait;
-    use StepRunnerUninstallTrait;
-
-    public function installStep1()
-    {
-        $schemaManager = $this->schemaManager();
-
-        $schemaManager->alterTable('xf_language', function (\XF\Db\Schema\Alter $table) {
-            $table->addColumn('xd_user_selectable', 'bool')->setDefault(1);
-        });
-    }
-
-    public function upgrade1000031Step1()
-    {
-        $schemaManager = $this->schemaManager();
-
-        $schemaManager->alterTable('xf_language', function (\XF\Db\Schema\Alter $table) {
-            $table->renameColumn('user_selectable', 'xd_user_selectable')->type('bool')->setDefault(1);
-        });
-
-        $schemaManager->alterTable('xf_language', function (\XF\Db\Schema\Alter $table) {
-//            $table->changeColumn('xd_user_selectable', 'bool')->setDefault(1);
-        });
-    }
-
-    public function uninstallStep1()
-    {
-        $schemaManager = $this->schemaManager();
-
-        $schemaManager->alterTable('xf_language', function (\XF\Db\Schema\Alter $table) {
-            $table->dropColumns(['xd_user_selectable']);
-        });
-    }
-}
->>>>>>> origin/master
