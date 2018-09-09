b87b04ed56c6daa70993ddefc7a87c066fff0f6f
McAtze <info@it-maku.com>
2018-09-09 11:38:05 +0200

    Update Setup.php

--- upload/src/addons/XenDACH/LimitLanguageUsage/Setup.php
+++ upload/src/addons/XenDACH/LimitLanguageUsage/Setup.php
@@ -7,36 +7,36 @@ use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
 use XF\AddOn\StepRunnerUpgradeTrait;
 
-class Setup extends AbstractSetup
-{
+class Setup extends AbstractSetup {
+	
 	use StepRunnerInstallTrait;
 	use StepRunnerUpgradeTrait;
 	use StepRunnerUninstallTrait;
 	
 	public function installStep1()
-    {
-        $schemaManager = $this->schemaManager();
+    	{
+        	$schemaManager = $this->schemaManager();
 
-        $schemaManager->alterTable('xf_language', function(\XF\Db\Schema\Alter $table) {
-            $table->addColumn('xd_user_selectable', 'bool')->setDefault(1);
-        });
-    }
+        	$schemaManager->alterTable('xf_language', function(\XF\Db\Schema\Alter $table) {
+            		$table->addColumn('xd_user_selectable', 'bool')->setDefault(1);
+        	});
+    	}
 
-    public function upgrade1000031Step1()
-    {
-        $schemaManager = $this->schemaManager();
+	public function upgrade1000031Step1()
+	{
+		$schemaManager = $this->schemaManager();
 
-        $schemaManager->alterTable('xf_language', function(\XF\Db\Schema\Alter $table) {
-            $table->renameColumn('user_selectable', 'xd_user_selectable')->type('bool')->setDefault(1);
-        });
-    }
+		$schemaManager->alterTable('xf_language', function(\XF\Db\Schema\Alter $table) {
+	    		$table->renameColumn('user_selectable', 'xd_user_selectable')->type('bool')->setDefault(1);
+		});
+	}
 
-    public function uninstallStep1()
-    {
-        $schemaManager = $this->schemaManager();
+	public function uninstallStep1()
+	{
+		$schemaManager = $this->schemaManager();
 
-        $schemaManager->alterTable('xf_language', function(\XF\Db\Schema\Alter $table) {
-            $table->dropColumns(['xd_user_selectable']);
-        });
-    }
+		$schemaManager->alterTable('xf_language', function(\XF\Db\Schema\Alter $table) {
+	    		$table->dropColumns(['xd_user_selectable']);
+		});
+	}
 }
